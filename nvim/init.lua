05b50129a9178b897d9026a8ac108b93a725781d
TheStarryNight1889 <30705821+TheStarryNight1889@users.noreply.github.com>
2024-03-26 01:18:56 +0000

    updated

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,23 @@
+
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 require('plugins')
 require('ls')
 local builtin = require('telescope.builtin')
 
 -- Theme
-vim.cmd.colorscheme "catppuccin-latte"
+vim.cmd.colorscheme "catppuccin-mocha"
 -- Relative line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -22,12 +36,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.api.nvim_set_keymap(
-  "n",
-  "<space>fb",
-  ":Telescope file_browser<Enter>",
-  { noremap = true }
-)
+vim.keymap.set('n', '<leader>ee',':E<CR>', {})
 
 -- Bind 'jk' to enter command mode in insert mode
 vim.api.nvim_set_keymap('i', 'jk', '<Esc>', {noremap = true})
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,13 +1,11 @@
-vim.cmd [[packadd packer.nvim]]
-require("telescope").load_extension "file_browser"
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
+  	'nvim-telescope/telescope.nvim',
+  	requires = { {'nvim-lua/plenary.nvim'} }
+	}
     use { "catppuccin/nvim", as = "catppuccin" }
     use 'neovim/nvim-lspconfig'
     use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
